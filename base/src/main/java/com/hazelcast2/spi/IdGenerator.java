98fea6813f55538ab36fb50dfc1ed343255dda4c
Peter Veentjer <alarmnummer@gmail.com>
2014-03-15 21:10:36 +0200

    Minor tweaks

--- base/src/main/java/com/hazelcast2/spi/IdGenerator.java
+++ base/src/main/java/com/hazelcast2/spi/IdGenerator.java
@@ -5,6 +5,9 @@ package com.hazelcast2.spi;
  *
  * This implementation is not threadsafe. It can be used by sectors since they will be executed
  * by a single thread at any given moment.
+ *
+ * When we are going to do id compression, e.g. instead of needing to use 64 bit, 32 bit is used
+ * for low addresses, it is good to have that logic here.
  */
 public class IdGenerator {
 
